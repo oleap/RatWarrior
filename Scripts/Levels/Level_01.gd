7525b0b4ebd08cf60db09da24b0273faf68dc40a
Sander Løtuft <sanderlot00@gmail.com>
2024-01-27 20:10:12 +0100

    Added main and pause menu

---
+++ Scripts/Levels/Level_01.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+@onready var pauseMenu = $UserInterface/Pause
+
+var paused = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("Pause"):
+		Pause()
+
+func Pause():
+	if paused:
+		pauseMenu.show()
+		Engine.time_scale = 0
+	else:
+		pauseMenu.hide()
+		Engine.time_scale = 1
+	paused = !paused
---
+++ Scripts/Menu/main_menu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Levels/Level_01.tscn")
+
+func _on_tutorial_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Menu/Tutorial.tscn")
+
+func _on_exit_pressed():
+	get_tree().quit()
